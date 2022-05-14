bfe82f527795e4a05ba978cb39855935dc83dc17
weichen3 <weichen3@uvic.ca>
2022-05-14 12:13:39 -0700

    Implemented setImageUrl

--- src/main/java/com/wc/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/wc/server/service/implementation/ServerServiceImpl.java
@@ -1,18 +1,22 @@
 package com.wc.server.service.implementation;
 
-import com.wc.server.enumeration.Status;
 import com.wc.server.model.Server;
 import com.wc.server.repo.ServerRepo;
 import com.wc.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
+import java.util.Random;
+
+import static com.wc.server.enumeration.Status.SERVER_DOWN;
+import static com.wc.server.enumeration.Status.SERVER_UP;
+import static org.springframework.data.domain.PageRequest.of;
 
 @RequiredArgsConstructor
 @Service
@@ -33,32 +37,38 @@ public class ServerServiceImpl implements ServerService {
         log.info("Pinging this server at IP address: {}", ipAddress);
         Server server = serverRepo.findByIpAddress(ipAddress);
         InetAddress address = InetAddress.getByName(ipAddress);
-        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
         serverRepo.save(server);
         return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by id: {}", id);
+        serverRepo.deleteById(id);
+        return Boolean.TRUE;
     }
 
     private String setServerImageUrl() {
-        return null;
+        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
