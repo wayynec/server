39208cf426bf30c4d77b407c1a10e9b6b3885757
weichen3 <weichen3@uvic.ca>
2022-05-14 14:19:43 -0700

    Finished implementing apis

--- src/main/java/com/wc/server/ServerApplication.java
+++ src/main/java/com/wc/server/ServerApplication.java
@@ -1,13 +1,28 @@
 package com.wc.server;
 
+import com.wc.server.enumeration.Status;
+import com.wc.server.model.Server;
+import com.wc.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.60", "Linux", "16GB", "PC",
+					"http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.77", "Windows", "16GB", "PC",
+					"http://localhost:8080/server/image/server2.png", Status.SERVER_UP));
+		};
 	}
 
 }
